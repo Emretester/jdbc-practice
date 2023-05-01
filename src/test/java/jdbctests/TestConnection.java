9ad13cf578b3e827be74c90af7218dd8332c21d9
Emretester <emreyeni2019@gmail.com>
2023-05-01 11:57:16 +0300

    Added all classes

--- src/test/java/jdbctests/TestConnection.java
+++ src/test/java/jdbctests/TestConnection.java
@@ -5,14 +5,13 @@ import java.sql.*;
 public class TestConnection {
 
     public static void main(String[] args) throws SQLException {
-
-        String dbUrl = "jdbc:oracle:thin:@3.83.86.89:1521:XE";
+        String dbUrl = "jdbc:oracle:thin:@44.202.119.26:1521:XE";
         String dbUsername = "hr";
         String dbPassword = "hr";
 
         Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
         Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("select * from regions");
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM regions");
 
         //once you set up connection default pointer looks for 0
         //next() --> move pointer to first row
@@ -39,6 +38,7 @@ public class TestConnection {
             System.out.println(resultSet.getInt(1) + " - " + resultSet.getString(2));
 
         }
+
 
         //close connection
         resultSet.close();
